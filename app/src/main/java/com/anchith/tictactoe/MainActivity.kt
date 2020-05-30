39e172cce901adbb69ed79817052929e5bc7f6b9
Anchith <anchithacharya@gmail.com>
2020-05-30 16:28:29 +0530

    removed unused layouts, fragments added comments everywhere some other small modifications

--- app/src/main/java/com/anchith/tictactoe/MainActivity.kt
+++ app/src/main/java/com/anchith/tictactoe/MainActivity.kt
@@ -6,7 +6,7 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.runBlocking
 
-lateinit var buttonGrid: MutableList<Button>
+lateinit var buttonList: MutableList<Button>
 
 class MainActivity : AppCompatActivity() {
 	private var playerTurn: Boolean = true
@@ -16,51 +16,49 @@ class MainActivity : AppCompatActivity() {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.button)
 
-		buttonGrid = mutableListOf(findViewById<Button>(R.id.button_1_1))
-		buttonGrid.add(findViewById<Button>(R.id.button_1_2))
-		buttonGrid.add(findViewById<Button>(R.id.button_1_3))
-		buttonGrid.add(findViewById<Button>(R.id.button_2_1))
-		buttonGrid.add(findViewById<Button>(R.id.button_2_2))
-		buttonGrid.add(findViewById<Button>(R.id.button_2_3))
-		buttonGrid.add(findViewById<Button>(R.id.button_3_2))
-		buttonGrid.add(findViewById<Button>(R.id.button_3_1))
-		buttonGrid.add(findViewById<Button>(R.id.button_3_3))
+		buttonList = mutableListOf(findViewById<Button>(R.id.button_1_1))							//add all buttons to global buttonList for easy access throughout the class
+		buttonList.add(findViewById<Button>(R.id.button_1_2))
+		buttonList.add(findViewById<Button>(R.id.button_1_3))
+		buttonList.add(findViewById<Button>(R.id.button_2_1))
+		buttonList.add(findViewById<Button>(R.id.button_2_2))
+		buttonList.add(findViewById<Button>(R.id.button_2_3))
+		buttonList.add(findViewById<Button>(R.id.button_3_2))
+		buttonList.add(findViewById<Button>(R.id.button_3_1))
+		buttonList.add(findViewById<Button>(R.id.button_3_3))
 	}
 
-	fun start(view:View)
+	fun start(view:View)																			//view refers to "Start" button
 	{
-		view.visibility = View.GONE
-//		suspend { play((0..1).random() == 1) }
-		play(false)
+		view.visibility = View.GONE                                                                 //view's visibility will be updated:
+//		suspend { play((0..1).random() == 1) }                                                		//->immediately (parallel execution)
+		play((0..1).random() == 1)															//->after control returns from play() and exits start()
 	}
 
-	fun change(view:View)
+	fun click(view:View)
 	{
 		val button = view as Button
 
-		val mark = when(playerTurn)
+		if(button.text == "")																		//if button is empty:
 		{
-			true -> "X"
-			false -> "O"
+			button.text = when(playerTurn)															//	button =
+			{
+				true -> "X"																			//			 'X' if playerTurn is true (player clicked the button)
+				false -> "O"																		//			 'O' if playerTurn is false (AI clicked the button)
+			}																						//NOTE: 'X' and 'O' are fixed for player and AI respectively, for now.
 		}
 
-		if(button.text == "")
-		{
-			button.text = mark
-		}
-
-		if(playerTurn) turnEnded = true
+		if(playerTurn) turnEnded = true																//finished clicking button; turn is ended
 	}
 
-	private fun aiPlay()
+	private fun aiPlay()																			//click random button that is empty
 	{
 		while(true)
 		{
 			val num = (0..8).random()
 
-			if(buttonGrid[num].text == "")
+			if(buttonList[num].text == "")
 			{
-				buttonGrid[num].performClick()
+				buttonList[num].performClick()
 				break
 			}
 		}
@@ -68,61 +66,64 @@ class MainActivity : AppCompatActivity() {
 
 	private fun play(seed: Boolean)
 	{
-		playerTurn = seed
+		playerTurn = seed																			//so that initial move is randomly selected between player and AI
 
-		while(!win())
+		while(!win())																				//while there is no winner
 		{
-			if(playerTurn)
+			if(playerTurn)																			//if it's player's turn
 			{
-				turnEnded = false
+				turnEnded = false																	//turn has started
 
-				for(view in buttonGrid)
+				for(view in buttonList)
 				{
-					view.isClickable = true
-					view.requestLayout()
+					view.isClickable = true															//make all views clickable,
+					view.invalidate()																//and refresh the views
 				}
 
+				runBlocking { while(!turnEnded); }													//while turn has not ended (this will be changed by click()) ???
 
-				runBlocking { while(!turnEnded); }
+				for(view in buttonList)
+				{
+					view.isClickable = false														//make all the views unclickable again,
+					view.invalidate()																//and refresh them
+				}
 
-				for(view in buttonGrid)
-					view.isClickable = false
 			}
 
-			else aiPlay()
+			else aiPlay()																			//if not player's turn, let AI make its move
 
-			playerTurn = !playerTurn
+			playerTurn = !playerTurn																//change the next turn
 		}
 
 		val button = findViewById<Button>(R.id.le_button)
-		button.text = "Restart"
-		button.visibility = View.VISIBLE
+		button.text = "Restart"																		//if someone has won, display the "restart" button
+		button.visibility = View.VISIBLE															//TODO: show win message (who has won)
 	}
 
 	private fun win():Boolean
 	{
 		for(i in 0..8 step 3)
 		{
-			if(buttonGrid[i].text == buttonGrid[i+1].text
-				&& buttonGrid[i].text == buttonGrid[i+2].text)
-				if(buttonGrid[i].text != "") return true
+			if(buttonList[i].text == buttonList[i+1].text											//check if all buttons in row have same symbol
+				&& buttonList[i].text == buttonList[i+2].text)
+				if(buttonList[i].text != "") return true
 		}
 
 		for(i in 0..2)
 		{
-			if(buttonGrid[i].text == buttonGrid[i+3].text
-				&& buttonGrid[i].text == buttonGrid[i+6].text)
-				if(buttonGrid[i].text != "") return true
+			if(buttonList[i].text == buttonList[i+3].text											//check if all buttons in column have same symbol
+				&& buttonList[i].text == buttonList[i+6].text)
+				if(buttonList[i].text != "") return true
 		}
 
-		if(buttonGrid[0].text == buttonGrid[4].text
-			&& buttonGrid[4].text == buttonGrid[8].text)
-			if(buttonGrid[4].text != "") return true
+		if(buttonList[0].text == buttonList[4].text													//check if all buttons in left diagonal have same symbol
+			&& buttonList[4].text == buttonList[8].text)
+			if(buttonList[4].text != "") return true
 
-		if(buttonGrid[2].text == buttonGrid[4].text
-			&& buttonGrid[4].text == buttonGrid[6].text)
-			if(buttonGrid[4].text != "") return true
+		if(buttonList[2].text == buttonList[4].text													//check if all buttons in right diagonal have same symbol
+			&& buttonList[4].text == buttonList[6].text)
+			if(buttonList[4].text != "") return true
 
-		return false
+		return false																				//nobody has won yet
 	}
 }
