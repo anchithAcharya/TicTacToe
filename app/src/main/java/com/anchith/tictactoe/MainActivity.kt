dcbd54246ca4f04af0e191bff93527712fc6bbcb
Anchith <anchithacharya@gmail.com>
2020-06-03 20:56:06 +0530

    removed all comments because they were messy added option to choose between "X", "O" or random before pressing start button optimised async call added player object to better represent some values

--- app/src/main/java/com/anchith/tictactoe/MainActivity.kt
+++ app/src/main/java/com/anchith/tictactoe/MainActivity.kt
@@ -1,129 +1,244 @@
 package com.anchith.tictactoe
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.Button
+import android.view.ViewGroup
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 
-lateinit var buttonList: MutableList<Button>
+lateinit var buttonList: List<Button>
+lateinit var startButton: Button
+lateinit var spinner: Spinner
 
-class MainActivity : AppCompatActivity() {
-	private var playerTurn: Boolean = true
-	private var turnEnded: Boolean = false
+class MainActivity : AppCompatActivity()
+{
 
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-		setContentView(R.layout.button)
-
-		buttonList = mutableListOf(findViewById<Button>(R.id.button_1_1))							//add all buttons to global buttonList for easy access throughout the class
-		buttonList.add(findViewById<Button>(R.id.button_1_2))
-		buttonList.add(findViewById<Button>(R.id.button_1_3))
-		buttonList.add(findViewById<Button>(R.id.button_2_1))
-		buttonList.add(findViewById<Button>(R.id.button_2_2))
-		buttonList.add(findViewById<Button>(R.id.button_2_3))
-		buttonList.add(findViewById<Button>(R.id.button_3_2))
-		buttonList.add(findViewById<Button>(R.id.button_3_1))
-		buttonList.add(findViewById<Button>(R.id.button_3_3))
-	}
-
-	fun start(view:View)																			//view refers to "Start" button
+	object Player
 	{
-		view.visibility = View.GONE                                                                 //view's visibility will be updated:
-//		suspend { play((0..1).random() == 1) }                                                		//->immediately (parallel execution)
-		play((0..1).random() == 1)															//->after control returns from play() and exits start()
-	}
+		var isPlayerTurn: Boolean = true
+		var turnEnded: Boolean = false
+		var symbol: String = ""
 
-	fun click(view:View)
-	{
-		val button = view as Button
-
-		if(button.text == "")																		//if button is empty:
+		fun altSymbol(symbol: String = Player.symbol) = when (symbol)
 		{
-			button.text = when(playerTurn)															//	button =
+			"X" -> "O"
+			"O" -> "X"
+			else -> null
+		}
+	}
+
+	private val scope = CoroutineScope(Dispatchers.IO)
+
+	class MyMediaAdapter(private val context: Context,
+						 private val content: List<String?>,
+						 private val isImage: List<Boolean>) : BaseAdapter()
+	{
+		private val inflater = LayoutInflater.from(context)
+
+		override fun getItem(p0: Int): Any? = content[p0]
+
+		override fun getItemId(p0: Int): Long = 0
+
+		override fun getCount(): Int = isImage.size
+
+		@SuppressLint("InflateParams")
+		override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View
+		{
+			val view: View
+
+			if (isImage[p0])
 			{
-				true -> "X"																			//			 'X' if playerTurn is true (player clicked the button)
-				false -> "O"																		//			 'O' if playerTurn is false (AI clicked the button)
-			}																						//NOTE: 'X' and 'O' are fixed for player and AI respectively, for now.
+				view = inflater.inflate(R.layout.spinner_image, null) as ImageView
+				view.setImageDrawable(androidx.appcompat.content.res.AppCompatResources.getDrawable(
+					context,
+					R.drawable.random_icon_24
+																								   )
+									 )
+			}
+			else
+			{
+				view = inflater.inflate(R.layout.spinner_text, null) as TextView
+				view.text = content[p0]
+			}
+
+			return view
+		}
+	}
+
+	override fun onCreate(savedInstanceState: Bundle?)
+	{
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+
+		buttonList =
+			listOf(findViewById(R.id.button_1_1),                            //add all buttons to global buttonList for easy access throughout the class
+				findViewById(R.id.button_1_2),
+				findViewById(R.id.button_1_3),
+				findViewById(R.id.button_2_1),
+				findViewById(R.id.button_2_2),
+				findViewById(R.id.button_2_3),
+				findViewById(R.id.button_3_1),
+				findViewById(R.id.button_3_2),
+				findViewById(R.id.button_3_3)
+				  )
+
+		startButton = findViewById(R.id.le_button)
+		spinner = findViewById(R.id.spinner)
+
+		val spinner = findViewById<Spinner>(R.id.spinner)
+		val adapter = MyMediaAdapter(this, listOf("X", "O", null), listOf(false, false, true))
+		spinner.adapter = adapter
+		spinner.setSelection(2)
+	}
+
+	fun start(view: View)
+	{
+		for (button in buttonList)
+		{
+			button.text = ""
+			button.visibility = View.VISIBLE
 		}
 
-		if(playerTurn) turnEnded = true																//finished clicking button; turn is ended
+		view.visibility = View.GONE
+		spinner.visibility = View.GONE
+
+		scope.launch { play(playAs = spinner.selectedItem?.toString()) }
 	}
 
-	private fun aiPlay()																			//click random button that is empty
+	fun click(view: View)
 	{
-		while(true)
+		for (button in buttonList)
+		{
+			button.isClickable = false
+			button.postInvalidate()
+		}
+
+		val button = view as Button
+
+		if (button.text == "")
+		{
+			button.text = when (Player.isPlayerTurn)
+			{
+				true -> Player.symbol
+				false -> Player.altSymbol()
+			}
+		}
+		else Player.isPlayerTurn = !Player.isPlayerTurn
+
+		Player.turnEnded = true
+	}
+
+	private suspend fun aiPlay()
+	{
+		while (true)
 		{
 			val num = (0..8).random()
 
-			if(buttonList[num].text == "")
+			if (buttonList[num].text == "")
 			{
-				buttonList[num].performClick()
+				withContext(Dispatchers.Main.immediate)
+				{
+					buttonList[num].performClick()
+					buttonList[num].invalidate()
+				}
+
 				break
 			}
 		}
 	}
 
-	private fun play(seed: Boolean)
+	private suspend fun play(seed: Boolean = ((0..1).random() == 1), playAs: String?)
 	{
-		playerTurn = seed																			//so that initial move is randomly selected between player and AI
+		Player.symbol = playAs ?: listOf("X", "O").random()
+		Player.isPlayerTurn = seed
 
-		while(!win())																				//while there is no winner
+		while (!withContext(MainScope().coroutineContext) { win() })
 		{
-			if(playerTurn)																			//if it's player's turn
+			if (Player.isPlayerTurn)
 			{
-				turnEnded = false																	//turn has started
+				Player.turnEnded = false
 
-				for(view in buttonList)
+				withContext(Dispatchers.Main.immediate)
 				{
-					view.isClickable = true															//make all views clickable,
-					view.invalidate()																//and refresh the views
+					for (view in buttonList)
+					{
+						view.isClickable = true
+						view.invalidate()
+					}
 				}
 
-				runBlocking { while(!turnEnded); }													//while turn has not ended (this will be changed by click()) ???
-
-				for(view in buttonList)
-				{
-					view.isClickable = false														//make all the views unclickable again,
-					view.invalidate()																//and refresh them
-				}
-
+				while (!Player.turnEnded) delay(1000)
 			}
-
-			else aiPlay()																			//if not player's turn, let AI make its move
-
-			playerTurn = !playerTurn																//change the next turn
+			else aiPlay()
+			Player.isPlayerTurn = !Player.isPlayerTurn
 		}
 
-		val button = findViewById<Button>(R.id.le_button)
-		button.text = "Restart"																		//if someone has won, display the "restart" button
-		button.visibility = View.VISIBLE															//TODO: show win message (who has won)
+		withContext(Dispatchers.Main.immediate)
+		{
+			startButton.text = getString(R.string.restart_label)
+
+			spinner.visibility = View.VISIBLE
+			startButton.visibility = View.VISIBLE
+		}
 	}
 
-	private fun win():Boolean
+	private fun win(): Boolean
 	{
-		for(i in 0..8 step 3)
+		val toast = Toast.makeText(applicationContext, "", Toast.LENGTH_LONG)
+
+		for (i in 0..8 step 3)
 		{
-			if(buttonList[i].text == buttonList[i+1].text											//check if all buttons in row have same symbol
-				&& buttonList[i].text == buttonList[i+2].text)
-				if(buttonList[i].text != "") return true
+			if (buttonList[i].text == buttonList[i + 1].text
+				&& buttonList[i].text == buttonList[i + 2].text)
+				if (buttonList[i].text != "")
+				{
+					toast.setText("${buttonList[i].text} wins!")
+					toast.show()
+					return true
+				}
 		}
 
-		for(i in 0..2)
+		for (i in 0..2)
 		{
-			if(buttonList[i].text == buttonList[i+3].text											//check if all buttons in column have same symbol
-				&& buttonList[i].text == buttonList[i+6].text)
-				if(buttonList[i].text != "") return true
+			if (buttonList[i].text == buttonList[i + 3].text
+				&& buttonList[i].text == buttonList[i + 6].text)
+				if (buttonList[i].text != "")
+				{
+					toast.setText("${buttonList[i].text} wins!")
+					toast.show()
+					return true
+				}
 		}
 
-		if(buttonList[0].text == buttonList[4].text													//check if all buttons in left diagonal have same symbol
+		if (buttonList[0].text == buttonList[4].text
 			&& buttonList[4].text == buttonList[8].text)
-			if(buttonList[4].text != "") return true
+			if (buttonList[4].text != "")
+			{
+				toast.setText("${buttonList[4].text} wins!")
+				toast.show()
+				return true
+			}
 
-		if(buttonList[2].text == buttonList[4].text													//check if all buttons in right diagonal have same symbol
+		if (buttonList[2].text == buttonList[4].text
 			&& buttonList[4].text == buttonList[6].text)
-			if(buttonList[4].text != "") return true
+			if (buttonList[4].text != "")
+			{
+				toast.setText("${buttonList[4].text} wins!")
+				toast.show()
+				return true
+			}
 
-		return false																				//nobody has won yet
+		if (buttonList.all { it.text != "" })
+		{
+			toast.setText("Draw!")
+			toast.show()
+			return true
+		}
+
+		return false
 	}
 }
